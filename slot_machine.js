e1a36efb4d3b0d16ab9661197352d5d37e9d5dd0
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-07-12 11:07:57 +0100

    Update slot_machine.js
    Copied the deposit_money function and adjusted the input required so that the user can enter the amount of lines that wish to bet on, Updated some comments and removed the console.log at the bottom as was only there for testing purposes

--- slot_machine.js
+++ slot_machine.js
@@ -1,3 +1,4 @@
+//Prompt the User
 const prompt = require("prompt-sync")();
 
 //Create a Fucntion to Deposit the Money
@@ -14,13 +15,34 @@ function deposit_money(){
         if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
             console.log("Invalid deposit amount, try again.");
         } else {
-            //Returns the Successful Bet to the User
+            //Returns the Successful Bet
             return numberDepositAmount;
         }
     }
 };
+//define a function to get the number of lines
+function getNumberOfLines(){
+    //Loop Until the user enters a valid amount
+    while (true){
+        //Ask the user for the amount of lines that wish to bet on
+        const lines = prompt("Enter the number of lines you wish to bet on(1-3): ");
+
+        //Convert the line amount from a String into a Number
+        const numberOfLines = parseFloat(lines);
+
+        //Check if the Number is valid and if not print invalid
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >= 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            //Returns the amount of lines
+            return numberOfLines;
+        }
+    }
+}
+
 
 //Sets depositAmount Equal to what was returns in the Function
 const depositAmount = deposit_money();
-//Print the Correct Amount
-console.log(depositAmount)
+
+//Sets Number of Lines equal to the amount that was returned in the function
+const numberOfLines = getNumberOfLines();
