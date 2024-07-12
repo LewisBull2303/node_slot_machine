8b262c3fc1fa90064ec26c0f55ccbe9ceeca039f
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-07-12 10:59:10 +0100

    Update slot_machine.js
    Looped the deposit_money function until the User enters a Valid Amount

--- slot_machine.js
+++ slot_machine.js
@@ -1,8 +1,8 @@
-//Prompt the User
 const prompt = require("prompt-sync")();
 
 //Create a Fucntion to Deposit the Money
 function deposit_money(){
+    //Loop Until the user enters a valid amount
     while (true){
         //Ask the user for a deposit ammount
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -13,9 +13,14 @@ function deposit_money(){
         //Check if the Number is valid and if not print invalid 
         if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
             console.log("Invalid deposit amount, try again.");
+        } else {
+            //Returns the Successful Bet to the User
+            return numberDepositAmount;
         }
     }
 };
 
-//Run the Function
-deposit_money();
+//Sets depositAmount Equal to what was returns in the Function
+const depositAmount = deposit_money();
+//Print the Correct Amount
+console.log(depositAmount)
