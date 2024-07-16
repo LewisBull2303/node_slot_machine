232cad9d2802f8b55829d75ffdf543e9a144393c
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-07-16 15:40:40 +0100

    Update slot_machine.js
    Bug fixed the spin function as it was not printing correctly, added a new function for transposing the symbols to the correct positions for the user to see if they have won

--- slot_machine.js
+++ slot_machine.js
@@ -86,30 +86,43 @@ function getBet(balance, lines){
 function spin(){
     const symbols = [];
     //for all of the symbols and the count of the symbols count variable
-    for (const[symbol, count] of Object.entries(SYMBOLS_COUNT)){
-        for (let i = 0; i < count; i++){
-            //push the symbols to the list to store all possible symbols
-            symbols.push(symbol);
-        }
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+      for (let i = 0; i < count; i++) {
+        //push the symbols to the list to store all possible symbols
+        symbols.push(symbol);
+      }
     }
-
     //init the slot machine rows
-    const reels = [[], [], []];
-    for (let i = 0; i < COLS; i++){
-        reels.push([]);
-        const reelSymbols = [...symbols];
-        for (let j = 0; j < ROWS; j++){
-            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
-            const selectedSymbol = reelSymbols[randomIndex];
-            reels[i].push(selectedSymbol);
-            reelSymbols.splice(randomIndex, 1);
-        }
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+      reels.push([]);
+      //copy all of the symbols to a new list
+      const reelSymbols = [...symbols];
+      for (let j = 0; j < ROWS; j++) {
+        //randomy generate the rows based on all the symbols available
+        const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        //get rid of the selected symbol so it cannot be selected again
+        reelSymbols.splice(randomIndex, 1);
+      }
     }
+  
     return reels;
-}
+  };
 
-const reels = spin();
-console.log(reels)
+function transpose(reels){
+    const rows = [];
+  
+    for (let i = 0; i < ROWS; i++) {
+      rows.push([]);
+      for (let j = 0; j < COLS; j++) {
+        rows[i].push(reels[j][i]);
+      }
+    }
+  
+    return rows;
+  };
 
 //Sets depositAmount Equal to what was returns in the Function
 let balance = deposit_money();
@@ -119,3 +132,8 @@ const numberOfLines = getNumberOfLines();
 
 //Gets the users total bet based on how much they put in the balance and the number of lines
 const bet = getBet(balance, numberOfLines);
+
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows);
